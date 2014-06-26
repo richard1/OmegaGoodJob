5a614f02aaf72537ef878d4887dfb00932eea555
Richard Lin <richwierdo@gmail.com>
2014-06-26 00:29:26 -0700

    v1.0: theme/skin, audio, sora sprite

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var speed = 100;
 var score = 0;
 var scoreText;
 var numStars = 0;
+var oldFrame = 1;
 
 // keyboard codes for key events
 var KEYCODE_A = 65;
@@ -14,10 +15,13 @@ var KEYCODE_SPACE = 32;
 var main_state = {
 
 	preload: function() {
-		game.load.image('sky', 'assets/sky.png');
-		game.load.image('ground', 'assets/platform.png');
-		game.load.image('star', 'assets/star.png');
-		game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+		game.load.image('sky', 'assets/bg.png');
+		game.load.image('ground', 'assets/platform2.png');
+		game.load.image('star', 'assets/heart.png');
+		//game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+		game.load.spritesheet('dude', 'assets/sorasprite.png', 80, 80);
+		
+		game.load.audio('sfx', 'assets/shoot.wav');
 	},
 
 	create: function() {
@@ -48,15 +52,21 @@ var main_state = {
 		player.body.collideWorldBounds = true;
  
 		// player's two animations, walking left and right
+		/*
 		player.animations.add('left', [0, 1, 2, 3], 10, true);
 		player.animations.add('right', [5, 6, 7, 8], 10, true);
+		*/
+		
+		player.frame = 1;
+		player.animations.add('left', [1], 10, true);
+		player.animations.add('right', [0], 10, true);
 	
 		cursors = game.input.keyboard.createCursorKeys();
 	
 		stars = game.add.group();
 		stars.enableBody = true;
 	
-		scoreText = game.add.text(16, 16, 'Stars: ' + numStars, { fontSize: '32px', fill: '#000' });
+		//scoreText = game.add.text(16, 16, 'Stars: ' + numStars, { fontSize: '32px', fill: '#000' });
 		
 		mouse = new Phaser.Mouse(game);
 		mouse.start();
@@ -64,6 +74,8 @@ var main_state = {
 		
 		keyboard = new Phaser.Keyboard(game);
 		keyboard.start();
+		
+		fx = game.add.audio('sfx');
 	},
 
 	update: function() {
@@ -77,19 +89,21 @@ var main_state = {
 			//  Move to the left
 			player.body.velocity.x = -150 - speed;
  
-			player.animations.play('left');
+			//player.animations.play('left');
+			player.frame = 1;
 		}
 		else if (cursors.right.isDown || keyboard.isDown(KEYCODE_D)) {
 			//  Move to the right
 			player.body.velocity.x = 150 + speed;
  
-			player.animations.play('right');
+			//player.animations.play('right');
+			player.frame = 0;
 		}
 		else {
 			//  Stand still
 			player.animations.stop();
- 
-			player.frame = 4;
+ 			
+			player.frame = oldFrame;
 		}
 	
 		//  Allow the player to jump if they are touching the ground.
@@ -103,20 +117,30 @@ var main_state = {
 		game.physics.arcade.collide(stars, stars);
 	
 		//game.physics.arcade.overlap(player, stars, this.collectStar, null, this);
+		oldFrame = player.frame;
 	},
 	
 	mouseUp: function() {
+		fx.play();
 		var x = mouse.event.screenX - 57;
 		var y = mouse.event.screenY - 93;
 		var playerX = player.body.right - player.body.width * 0.5;
 		var playerY = player.body.bottom - player.body.height * 0.5;
 		numStars++;
-		scoreText.text = 'Stars: ' + numStars;
+		//scoreText.text = 'Stars: ' + numStars;
 
-		if(x > playerX)		player.animations.play('right');
-		else				player.animations.play('left');
-		
-		var star = stars.create(playerX, playerY, 'star');
+		if(x > playerX) {
+			//player.animations.play('right');
+			player.frame = 0;
+			oldFrame = 0;
+		}
+		else {
+			//player.animations.play('left');
+			player.frame = 1;
+			oldFrame = 1;
+		}
+				
+		var star = stars.create(playerX, playerY - 20, 'star');
 
 		var hyp = Math.sqrt( (x - playerX) * (x - playerX) +
 			(y - playerY) * (y - playerY) );
@@ -128,6 +152,14 @@ var main_state = {
 		star.body.collideWorldBounds = true;
 		game.physics.enable(star, Phaser.Physics.ARCADE);
 		star.body.bounce.setTo(1, 1);
+		//star.body.angularVelocity = 20;
+		var diffX = x - playerX;
+		var diffY = y - playerY;
+		
+		var offset = 90;
+		if(diffX < 0) offset += 180;
+		//if(diffY < 0) offset ;
+		star.angle = Math.atan(diffY / diffX) * 180 / Math.PI + offset;
 	},
 
 	collectStar: function(player, star) {
@@ -137,7 +169,7 @@ var main_state = {
  
 		// add and update the score
 		score += 10;
-		scoreText.text = 'Score: ' + score;
+		//scoreText.text = 'Score: ' + score;
 	
 		speed += 10;
 	}
